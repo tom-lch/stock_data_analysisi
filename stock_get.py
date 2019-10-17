68ad521d4e22c7e1c98825f470adaf051cc8f215
LiChao <lichao_cs@163.com>
2019-10-17 23:26:45 +0800

    使用多线程实现抓取数据

--- stock_get.py
+++ stock_get.py
@@ -2,10 +2,12 @@ import tushare as ts
 import time
 import pandas as pd
 import numpy as np
-
+import threading
+#from threading import Thread, Lock, current_thread
+# 此版本使用多线程
 ts.set_token('20f64f9be7cff644f8a71b99a7eb4853b2c37e5f72e6128f2055fba4')
 pro = ts.pro_api()
-class Stock_data(object):
+class Stock_data():
     def __init__(self, up_time, end_date):
         self.up_time = up_time
         self.end_date = end_date
@@ -18,21 +20,43 @@ class Stock_data(object):
         stock_lists = df[df['list_date']<self.up_time]
         return stock_lists
 
-    def get_daily_data(self, ts_code):
-        stock_daily_lists = pro.daily(ts_code=ts_code, start_date=self.up_time, end_date=self.end_date)
-        df = pro.daily_basic(ts_code=ts_code, start_data=self.up_time, end_date=self.end_date)
-        return pd.merge(stock_daily_lists, df, on=['ts_code', 'trade_date', 'close'])
-
-    def data_group(self):
-        stock_lists = self.get_stock_list()['ts_code']
+    def get_daily_data(self, stock_list, L):
+        lock = threading.Lock()
+        while True:
+            if stock_list == []:
+                break
+            lock.acquire()
+            ts_code = stock_list.pop()
+            lock.release()  
+            print(f'{threading.current_thread().getName()}获取{ts_code}了，准备获取数据')
+            stock_daily_lists = pro.daily(ts_code=ts_code, start_date=self.up_time, end_date=self.end_date)
+            df = pro.daily_basic(ts_code=ts_code, start_data=self.up_time, end_date=self.end_date)
+            data_list = pd.merge(stock_daily_lists, df, on=['ts_code', 'trade_date', 'close'])
+            print(f'{threading.current_thread().getName()}准备存放{ts_code}的数据')
+            lock.acquire()
+            L.append(data_list)
+            print(f'{ts_code} is OK!')
+            lock.release()
+        return L
+    def thread_pool(self, stock_list):
         L = []
-        for stock_code in stock_lists:
-            stock_daily = self.get_daily_data(stock_code)
-            L.append(stock_daily)
-            print(f"{stock_code}'s data is OK!!")
-        print('进行数据合并。')
+        thread_name = ['1号线程', '2号线程', '3号线程', '4号线程', '5号线程']
+        for name in thread_name:
+            print(f'-------{name}启动了--------')
+            td = threading.Thread(target=self.get_daily_data, name=name, args=(stock_list, L))
+            td.start()
+        td.join()         
+        print('线程结束') 
+        return L
+    def data_group(self):
+        print('1')
+        stock_list = self.get_stock_list()['ts_code']
+        print('2')
+        stock_list = list(stock_list)
+        print('开始调用进程')
+        L = self.thread_pool(stock_list)
+        print('结束')
         return pd.concat(L)
-
 if __name__ == '__main__':   
     lists = Stock_data(up_time='20100101', end_date='20191016').data_group()
     lists.to_csv('stock_data.csv')
